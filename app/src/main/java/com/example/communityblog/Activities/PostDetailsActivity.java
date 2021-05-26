26ef7828b24f096579dcad3a82558d8348a6bddb
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-05-26 23:13:00 +0300

    Designed and coded layout for Post Details Activity

---
+++ app/src/main/java/com/example/communityblog/Activities/PostDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.communityblog.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.communityblog.R;
+
+public class PostDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+}
